1da144681508d5167160e9e0ec82a5fcafaf9a11
vladharl <50050915+vladharl@users.noreply.github.com>
2022-02-05 16:12:00 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,8 @@ export default function Home() {
     <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
 <script>
   hbspt.forms.create({
-	  region: "na1", 
 	  portalId: "21400381",
+	  region: "na1",
 	  formId: "bf045340-9975-452f-9435-c99515e7d48e"
 	  });
 </script>
