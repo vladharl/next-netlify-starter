a92ab444ae8291f533661070e2ed966251a42d7c
vladharl <50050915+vladharl@users.noreply.github.com>
2022-02-05 15:55:01 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,14 @@ export default function Home() {
       <main>
         <Header title="Vote Vlad" />
     </main>
+    <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
+<script>
+  hbspt.forms.create({
+	region: "na1",
+	portalId: "21400381",
+	formId: "bf045340-9975-452f-9435-c99515e7d48e"
+});
+</script>
 
       <Footer />
     </div>
